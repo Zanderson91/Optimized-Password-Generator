3769d00c3db62b6516dee4882e22677da90bf370
Zack Anderson <zack.anderson2011@gmail.com>
2021-06-04 08:37:36 -0500

    Added Math.floor function to establish random number for numbArray

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function writePassword() {
 
     const uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     const lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    const numbArray[]
+    const numbArray = Math.floor(Math.random() * choices.length)
     const characterArray[]
 
 
