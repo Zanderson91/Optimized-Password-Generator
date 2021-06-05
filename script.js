18e09e89ec01b514dd0ff7c3aafc05933a5059d0
Zack Anderson <zack.anderson2011@gmail.com>
2021-06-05 17:32:37 -0500

    Making edits to include new .getelementByID..WIP

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
+const uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const numbArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const characterArray = ["!", "”", "#", "$", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "["];
+
+/*
+const passwordFinal = document.getElementById("password")
+const length = document.getElementById("length")
+const numbers = document.getElementById("numbers")
+const characters = document.getElementById("characters")
+*/
+
+// Write password to the #password input
+function writePassword() {
+    const password = generatePassword();
+    const passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
+
+
+/*
+
 function generatePassword() {
+prompt("How many characters should the password be? 8-128?");
+const number = confirm("Would you like numbers in your password?");
+const upper = confirm("Would you like Uppercase letters in the password?");
+const lower = confirm("Would you like lowercase letters in your password?");
+const character = confirm("Would you like to include special characters in your password?");
+const resultArray = [""]
 
-const uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-const lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-const numbArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-const characterArray = ["!", "”", "#", "$", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "["]
-
-prompt("How many characters should the password be? 8-128?")
-const number = prompt("Would you like numbers in your password?")
-const upper = prompt("Would you like Uppercase letters in the password?")
-const lower = prompt("Would you like lowercase letters in your password?")
-const character = prompt("Would you like to include special characters in your password?")
-
-let resultArray = []
+*/
 
 
 if (number) {
@@ -35,13 +52,9 @@ console.log(resultArray)
 
 
 
-// Write password to the #password input
-function writePassword() {
-    const password = generatePassword();
-    const passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-}
+    
+    ///Do I need to add this??
+    //const randomIndex = Math.floor(Math.random() * choices.length)
 
 
 
