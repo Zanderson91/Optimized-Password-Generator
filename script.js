d1ff70ee23f738e50edaecfff505fd33962eba3f
Zack Anderson <zack.anderson2011@gmail.com>
2021-06-06 08:45:13 -0500

    Created prompt for how many characters should be used in generatePass

--- script.js
+++ script.js
@@ -5,19 +5,22 @@ const uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "
 const lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const numbArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const characterArray = ["!", "”", "#", "$", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "["];
+const resultArray = [""]
 
 
 
 
-
-prompt("How many characters should the password be? 8-128?");
+const length = "";
 const number = confirm("Would you like numbers in your password?");
 const upper = confirm("Would you like Uppercase letters in the password?");
 const lower = confirm("Would you like lowercase letters in your password?");
 const character = confirm("Would you like to include special characters in your password?");
-const resultArray = [""]
+
+
 
 function generatePassword() {
+    const length = (prompt("How many characters should the password be? 8-128?"));
+
 
 
 
@@ -30,8 +33,6 @@ function writePassword() {
 }
 
 
-
-*/
 
 
 if (number) {
