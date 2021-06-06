1dc093ad23de79cf054228e12122f8d87fcfe2e9
Zack Anderson <zack.anderson2011@gmail.com>
2021-06-06 12:19:58 -0500

    WIP...array's are now called based on selection criteria

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ const uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "
 const lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const numbArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const characterArray = ["!", "”", "#", "$", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "["];
-const resultArray = [""]
+
+let resultArray = []
 
 
-
-
-const length = "";
+let passLength = (prompt("How many characters should the password be? 8-128?"));
 const number = confirm("Would you like numbers in your password?");
 const upper = confirm("Would you like Uppercase letters in the password?");
 const lower = confirm("Would you like lowercase letters in your password?");
@@ -19,46 +18,44 @@ const character = confirm("Would you like to include special characters in your 
 
 
 function generatePassword() {
-    const length = (prompt("How many characters should the password be? 8-128?"));
-
-for(length <= 8 || length >= 128) {
-    alert(Password length must be between 8 and 128 characters!);
-    const length = (prompt("How many characters should the password be? 8-128?"));
-}
-
-
-
-// Write password to the #password input
-function writePassword() {
-    const password = generatePassword();
-    const passwordText = document.querySelector("#password");
-    passwordText.value = password;
-}
-
-
-
+    let length = "";
+    for (i = 0; i < length; i++) {
+        finalPassword = finalPassword + resultArray[Math.floor(Math.random() * resultArray.length)];
+    }
+    console.log(finalPassword)
 
 if (number) {
     resultArray = resultArray.concat(numbArray)
-}
+};
+
 if (upper) {
     resultArray = resultArray.concat(uppercaseArray)
-}
+};
+
 if (lower) {
     resultArray = resultArray.concat(lowercaseArray)
-}
+};
+
 if (character) {
     resultArray = resultArray.concat(characterArray)
-}
+};
+
 console.log(resultArray)
+
 }
 
+let finalPassword = ""
 
 
 
-    
-    ///Do I need to add this??
-    //const randomIndex = Math.floor(Math.random() * choices.length)
+    //return finalPassword;
+
+// Write password to the #password input
+function writePassword() {
+    let password = generatePassword();
+    let passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
 
 
 
