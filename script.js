3f3754b47a4aced60fdbe5dd0062b3248c09657f
Zack Anderson <zack.anderson2011@gmail.com>
2021-06-07 13:57:50 -0500

    Added if alert for passwords that do not fit criteria

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-const generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 const uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -8,14 +8,18 @@ const characterArray = ["!", "”", "#", "$", "%", "&", "’", "(", ")", "*", "+
 
 
 function generatePassword() {
-    prompt ("How many characters should the password be? 8-128?");
+    let passLength = prompt ("How many characters should the password be? 8-128?");
     let number = confirm("Would you like numbers in your password?");
     let upper = confirm("Would you like Uppercase letters in the password?");
     let lower = confirm("Would you like lowercase letters in your password?");
     let character = confirm("Would you like to include special characters in your password?");
-    let resultArray = []
+    let resultArray = [];
     let finalPassword = '';
 
+    if (passLength < 8 || passLength > 128) {
+        alert("Password length must be between 8 and 128 characters!")
+        let passLength = (prompt("How many characters should the password be? 8-128?"));
+    }
     if (number) {
         resultArray = resultArray.concat(numbArray)
     };
@@ -34,8 +38,8 @@ function generatePassword() {
 
     console.log(resultArray)
 
-    for (var i = 1; i < length; i++) {
-        finalPassword = finalPassword + resultArray(Math.floor(Math.random() * resultArray.length));
+    for (var i = 0; i < length; i++) {
+        finalPassword = finalPassword + resultArray[Math.floor(Math.random() * resultArray.length)];
     }
     console.log(finalPassword)
     return finalPassword;
